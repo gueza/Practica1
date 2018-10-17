ca247a7f00e4854438a5fe40075def9e00fbc79c
Stephania Zg <stezague88@gmail.com>
2018-10-17 16:29:11 -0500

    Articles features

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+#Se almacenan todas las rutas
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "articles#index"
+  resources :articles
 end
